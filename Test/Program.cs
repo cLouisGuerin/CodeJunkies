2a3b07c2b5ef1ea967f7a422419b25473a07bc35
dennis511999 <dennis.engelen121084@student.leeuwenborgh.nl>
2017-10-04 10:52:16 +0200

    Test 1

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello ik ben Dennis");
+            Console.ReadLine();
         }
     }
 }
