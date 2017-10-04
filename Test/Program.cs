006c950d01cd0a4f188f05344a0821c6f353ca89
Michaelp211 <31918837+Michaelp211@users.noreply.github.com>
2017-10-04 11:14:17 +0200

    blablabla

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,6 +11,7 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello ik ben Dennis");
+            Console.WriteLine("Ja, dat zie ik");
             Console.ReadLine();
         }
     }
